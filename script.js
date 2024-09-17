36eab7abaf95711ee55980aa31f916225be3e9fb
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2024-09-16 21:31:08 -0300

    para usar o link do github pagess

---
+++ script.js
@@ -0,0 +1,33 @@
+let mouseX = 0;
+let mouseY = 0;
+let targetX = 0;
+let targetY = 0;
+const delay = 5; // Intervalo de atraso em milissegundos
+let lastUpdate = 0;
+
+document.addEventListener('mousemove', (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+});
+
+function updateMask(timestamp) {
+    if (!lastUpdate) lastUpdate = timestamp;
+    const progress = timestamp - lastUpdate;
+
+    if (progress > delay) {
+        // Suaviza o movimento com um atraso
+        targetX += (mouseX - targetX) * 0.1;
+        targetY += (mouseY - targetY) * 0.1;
+
+        const staticEffect = document.getElementById('staticEffect');
+        staticEffect.style.setProperty('--mouseX', `${targetX}px`);
+        staticEffect.style.setProperty('--mouseY', `${targetY}px`);
+
+        lastUpdate = timestamp;
+    }
+
+    requestAnimationFrame(updateMask);
+}
+
+// Inicia a atualização da máscara
+updateMask();
